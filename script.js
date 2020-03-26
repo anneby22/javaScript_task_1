40c97cf15ccf9fe946d48b2665b99fb757f2fcb8
Odo Anastecia Ebere <odoanastecia1@gmail.com>
2020-03-26 16:32:16 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let myName=("Odo Anastecia Ebere");
+console.log(myName);
+
+const myCourses=["HTML", "CSS", "JavaScript"];
+console.log(myCourses);
+
+const myData = [];
+
+for (let i = 1; i <= 200; i++) {
+	if (i%2 === 1) {
+		myData.push(i);
+	}else{
+
+	}
+} console.log(myData);
